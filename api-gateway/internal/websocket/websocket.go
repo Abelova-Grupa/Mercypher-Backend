b1cefbb1712e93aa42e2020c914109ef71090fb9
jelisavac-l <jelisavacluka63@gmail.com>
2025-05-20 15:59:24 +0200

    feat(api-gateway): Unmarshaling websocket messages.

--- api-gateway/internal/websocket/websocket.go
+++ api-gateway/internal/websocket/websocket.go
@@ -1,19 +1,27 @@
 package websocket
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
-	"sync"
+	// "sync"
 
 	"github.com/gorilla/websocket"
 )
 
+
+// Storing data as RawMessage so it can be unmarshaled according to the given type.
+type Envelope struct {
+    Type string          `json:"type"`
+    Data json.RawMessage `json:"data"` // defer decoding of data
+}
+
 // Map connections is of structure connection : isActive
 // mutex is used because maps aren't thread safe
-type WebSocketConnectionManager struct {
-	connections map[*websocket.Conn]bool
-	mu          sync.Mutex
-}
+// type WebSocketConnectionManager struct {
+// 	connections map[*websocket.Conn]bool
+// 	mu          sync.Mutex
+// }
 
 var Upgrader = websocket.Upgrader{
 	CheckOrigin: func(r *http.Request) bool {
@@ -22,9 +30,17 @@ var Upgrader = websocket.Upgrader{
 	},
 }
 
-func NewWebSocketManager() *WebSocketConnectionManager {
-	return &WebSocketConnectionManager{
-		connections: make(map[*websocket.Conn]bool),
+// func NewWebSocketManager() *WebSocketConnectionManager {
+// 	return &WebSocketConnectionManager{
+// 		connections: make(map[*websocket.Conn]bool),
+// 	}
+// }
+
+func Respond(conn *websocket.Conn, messageType int, response string) error {
+	if err := conn.WriteMessage(messageType, []byte(response)); err != nil {
+		return err	
+	} else {
+		return nil
 	}
 }
 
@@ -43,11 +59,26 @@ func HandleClient(conn *websocket.Conn) {
 
 		log.Printf("Received: %s", msg)
 
-		// Echo it back
-		err = conn.WriteMessage(messageType, msg)
-		if err != nil {
-			log.Println("Write error:", err)
-			break
+		// Unmarshal the message
+		var env Envelope
+		if err := json.Unmarshal(msg, &env); err != nil {
+			log.Println("Failed to unmarshall message!")
+			continue
 		}
+
+		// Get message type and act accordingly
+		switch env.Type {
+			case "message": 
+				if err := Respond(conn, messageType, "Message received!"); err != nil {
+					log.Println("Couldn't respond.")
+				}
+			default:
+				if err := Respond(conn, messageType, "Unknown message type!"); err != nil {
+					log.Println("Couldn't respond.")
+				}
+		}
+
+		// Echo it back
+		
 	}
 }
