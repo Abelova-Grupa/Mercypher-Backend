256ace716af16350c584ed46fb6253e19eae619b
jelisavac-l <jelisavacluka63@gmail.com>
2025-05-23 20:28:48 +0200

    feat(api-gateway): Implemented gRPC server on gateway.

--- api-gateway/go.mod
+++ api-gateway/go.mod
@@ -4,6 +4,8 @@ go 1.24.0
 
 require github.com/gorilla/websocket v1.5.3
 
+require google.golang.org/genproto/googleapis/rpc v0.0.0-20250218202821-56aae31c358a // indirect
+
 require (
 	github.com/bytedance/sonic v1.13.2 // indirect
 	github.com/bytedance/sonic/loader v0.2.4 // indirect
@@ -30,6 +32,7 @@ require (
 	golang.org/x/net v0.40.0 // indirect
 	golang.org/x/sys v0.33.0 // indirect
 	golang.org/x/text v0.25.0 // indirect
+	google.golang.org/grpc v1.72.1
 	google.golang.org/protobuf v1.36.6 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ api-gateway/internal/server/gateway_server.go
@@ -0,0 +1,57 @@
+package server
+
+import (
+	"io"
+	"log"
+
+	pb "github.com/Abelova-Grupa/Mercypher/api/internal/grpc"
+)
+
+type GatewayServer struct {
+	pb.UnimplementedGatewayServiceServer
+}
+
+func (s *GatewayServer) Stream(stream pb.GatewayService_StreamServer) error {
+	for {
+		req, err := stream.Recv()
+		if err == io.EOF {
+			return nil
+		}
+		if err != nil {
+			log.Printf("Stream recv error: %v", err)
+			return err
+		}
+
+		switch payload := req.Payload.(type) {
+
+		case *pb.GatewayRequest_ChatMessage:
+			msg := payload.ChatMessage
+			log.Printf("Chat message: %s -> %s: %s", msg.SenderId, msg.RecipientId, msg.Body)
+
+			// TODO: Forward to the correct routine
+
+			stream.Send(&pb.GatewayResponse{
+				Status: "ok",
+				Body:   "chat message forwarded",
+			})
+
+		case *pb.GatewayRequest_MessageStatus:
+			status := payload.MessageStatus
+			log.Printf("Status update: %s marked %s as %s", status.RecipientId, status.MessageId, status.Status)
+
+			// TODO: Forward to the correct routine
+
+			stream.Send(&pb.GatewayResponse{
+				Status: "ok",
+				Body:   "status update forwarded",
+			})
+
+		default:
+			log.Println("Unknown payload")
+			stream.Send(&pb.GatewayResponse{
+				Status: "error",
+				Body: "unknown payload",
+			})
+		}
+	}
+}
