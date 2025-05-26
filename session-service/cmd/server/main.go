87e777e3f80670c99093cd79c7125b1ba6692e5c
LukaDervisevic <lukadervisevic@gmail.com>
2025-05-26 09:28:14 +0200

    fix(tlsFix): #Fixing missing key and certification on railway

--- session-service/cmd/server/main.go
+++ session-service/cmd/server/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"crypto/tls"
 	"log"
 	"net"
+	"os"
 
 	"github.com/Abelova-Grupa/Mercypher/session-service/internal/db"
 	pb "github.com/Abelova-Grupa/Mercypher/session-service/internal/grpc/pb"
@@ -12,14 +14,36 @@ import (
 )
 
 func main() {
+	// Loading env variables from cloud
+	tlsCert := os.Getenv("TLS_CERT")
+	tlsKey := os.Getenv("TLS_KEY")
+
+	var creds credentials.TransportCredentials
+	var err error
+	// These variables are only store on the cloud
+	if tlsCert == "" || tlsKey == "" {
+		creds, err = credentials.NewServerTLSFromFile("../../internal/certs/server.crt", "../../internal/certs/server.key")
+		if err != nil {
+			log.Fatalf("Failed to load TLS keys: %v", err)
+		}
+	} else {
+		// Creating a certificate : key pair
+		cert, err := tls.X509KeyPair([]byte(tlsCert), []byte(tlsKey))
+		if err != nil {
+			log.Fatalf("Failed to generate x509 pair: %v", err)
+		}
+		// Creating tls configuration based on certificate pair
+		tlsConfig := &tls.Config{
+			Certificates: []tls.Certificate{cert},
+			MinVersion:   tls.VersionTLS12,
+		}
+		// Creating credentials
+		creds = credentials.NewTLS(tlsConfig)
+	}
 
 	listener, err := net.Listen("tcp", ":50052")
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
-	}
-	creds, err := credentials.NewServerTLSFromFile("../../internal/certs/server.crt", "../../internal/certs/server.key")
-	if err != nil {
-		log.Fatalf("Failed to load TLS keys: %v", err)
 	}
 
 	grpcServer := grpc.NewServer(grpc.Creds(creds))
