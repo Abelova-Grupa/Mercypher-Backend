9223eb2c6ad1d2c4a2af8486c9c407481c233d6e
jelisavac-l <jelisavacluka63@gmail.com>
2025-06-08 21:22:24 +0200

    feat(api-gateway): User client

--- api-gateway/go.mod
+++ api-gateway/go.mod
@@ -2,12 +2,15 @@ module github.com/Abelova-Grupa/Mercypher/api
 
 go 1.24.3
 
-require github.com/gorilla/websocket v1.5.3
+require (
+	github.com/Abelova-Grupa/Mercypher/user-service v0.0.0-20250608151330-8843e3bf3dd1
+	github.com/gorilla/websocket v1.5.3
+)
 
 require (
 	github.com/grpc-ecosystem/grpc-gateway/v2 v2.26.3 // indirect
-	google.golang.org/genproto/googleapis/api v0.0.0-20250303144028-a0af3efb3deb // indirect
-	google.golang.org/genproto/googleapis/rpc v0.0.0-20250303144028-a0af3efb3deb // indirect
+	google.golang.org/genproto/googleapis/api v0.0.0-20250324211829-b45e905df463 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20250324211829-b45e905df463 // indirect
 )
 
 require (
@@ -37,7 +40,7 @@ require (
 	golang.org/x/net v0.40.0 // indirect
 	golang.org/x/sys v0.33.0 // indirect
 	golang.org/x/text v0.25.0 // indirect
-	google.golang.org/grpc v1.72.1
+	google.golang.org/grpc v1.73.0
 	google.golang.org/protobuf v1.36.6
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ api-gateway/internal/clients/user_client.go
@@ -0,0 +1,80 @@
+package clients
+
+import (
+	"context"
+	"errors"
+
+	"github.com/Abelova-Grupa/Mercypher/api/internal/domain"
+	userpb "github.com/Abelova-Grupa/Mercypher/user-service/external/proto"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/protobuf/types/known/timestamppb"
+)
+
+type UserClient struct {
+	conn   *grpc.ClientConn
+	client userpb.UserServiceClient
+}
+
+// NewUserClient cretes a new client to a user service on the given address.
+//
+// Note:	The situation is the same as in NewMessageClient code. Even if the
+//
+//	connection fails or refuses it wont be registered. Only when sending
+//	messages to an unexisting address will the error be thrown.
+func NewUserClient(address string) (*UserClient, error) {
+	conn, err := grpc.NewClient(address, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		return nil, err
+	}
+
+	if conn == nil {
+		return nil, errors.New("Connection refused: nil")
+	}
+
+	client := userpb.NewUserServiceClient(conn)
+
+	return &UserClient{
+		conn:   conn,
+		client: client,
+	}, nil
+}
+
+func (c *UserClient) Close() error {
+	return c.conn.Close()
+}
+
+// Register method returns ID of the created user.
+func (c *UserClient) Register(user domain.User, password string) (string, error) {
+	response, err := c.client.Register(context.Background(),
+		&userpb.User{
+			ID:        user.UserId,
+			Username:  user.Username,
+			Email:     user.Email,
+			Password:  password,
+			CreatedAt: timestamppb.Now(),
+		})
+
+	if err != nil {
+		return "", err
+	}
+
+	return response.ID, nil
+}
+
+// Login method returns access token of the logged user
+func (c *UserClient) Login(user domain.User, password string, accessToken string) (string, error) {
+	response, err := c.client.Login(context.Background(),
+		&userpb.LoginRequest{
+			UserID:      user.UserId,
+			Username:    user.Username, // Redundant?
+			Password:    password,
+			AccessToken: accessToken,
+		})
+
+	if err != nil {
+		return "", err
+	}
+
+	return response.AccessToken, nil
+}
