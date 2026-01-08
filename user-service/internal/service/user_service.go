92673fe84e5dc5f1bb8f7915d92373a36b3c86c0
LukaDervisevic <lukadervisevic@gmail.com>
2026-01-08 14:13:37 +0100

    pushing onto feature branch so i can continue to work on desktop

--- user-service/internal/service/user_service.go
+++ user-service/internal/service/user_service.go
@@ -2,12 +2,16 @@ package service
 
 import (
 	"context"
+	"crypto/tls"
 	"errors"
+	"net/smtp"
+	"os"
 	"time"
 
 	pb "github.com/Abelova-Grupa/Mercypher/proto/user"
 	"github.com/Abelova-Grupa/Mercypher/user-service/internal/models"
 	"github.com/Abelova-Grupa/Mercypher/user-service/internal/repository"
+	"github.com/gin-gonic/gin"
 	"golang.org/x/crypto/bcrypt"
 	"google.golang.org/protobuf/types/known/timestamppb"
 )
@@ -39,12 +43,67 @@ func (s *UserService) Register(ctx context.Context, userPb *pb.User) (*pb.User, 
 
 	// Other fields are already stored in user struct
 	user.PasswordHash = string(hashed)
+	user.Validated = false
 
 	if err := s.repo.CreateUser(ctx, user); err != nil {
 		return nil, err
 	}
 
 	return convertUserToPb(user), nil
+}
+
+func (s *UserService) ValidateAccount(ctx context.Context, username string, authCode string) error {
+	if username == "" || authCode == "" {
+		return ErrInvalidParams
+	}
+	return s.repo.ValidateAccount(ctx, username, authCode)
+}
+
+func (s *UserService) SendEmail(ctx *gin.Context, toEmail string, username string) error {
+	appPass := os.Getenv("EMAIL_APP_PASS")
+	fromEmail := os.Getenv("EMAIL_CLIENT")
+	smtpServer := os.Getenv("SMTP_HOST")
+	tlsPort := os.Getenv("TLS_SMTP_PORT")
+
+	tlsConfig := &tls.Config{
+		InsecureSkipVerify: false,
+		ServerName:         smtpServer,
+	}
+
+	auth := smtp.PlainAuth("", fromEmail, appPass, smtpServer)
+
+	conn, err := tls.Dial("tcp", smtpServer+":"+tlsPort, tlsConfig)
+	if err != nil {
+
+	}
+
+	client, err := smtp.NewClient(conn, smtpServer)
+	if err != nil {
+
+	}
+
+	if err := client.Auth(auth); err != nil {
+
+	}
+
+	if err := client.Mail(fromEmail); err != nil {
+
+	}
+
+	if err := client.Rcpt(toEmail); err != nil {
+
+	}
+
+	wc, err := client.Data()
+	if err != nil {
+
+	}
+
+	_, err = wc.Write([]byte(message))
+	if err != nil {
+
+	}
+
 }
 
 func (s *UserService) Login(ctx context.Context, username string, password string) (bool, error) {
